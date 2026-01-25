7d58912feedd1183b48e4b0279b5c22fbce6d57a
Asif <asifansari94@gmail.com>
2026-01-25 20:52:15 +0530

    Added GameBoardViewModelTest.kt class GameBoardViewModelTest.kt with simple test

---
+++ app/src/main/java/com/example/tictactoeapp/presentation/viewmodel/GameBoardViewModel.kt
@@ -0,0 +1,16 @@
+package com.example.tictactoeapp.presentation.viewmodel
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class GameBoardViewModel: ViewModel() {
+
+    private var _board = MutableStateFlow(List(9) { "" }.toMutableList())
+    val board: StateFlow<List<String>>
+        get() = _board
+
+    fun play(move: Int) {
+        _board.value[move] = "X"
+    }
+}
