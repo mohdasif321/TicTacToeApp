2d40c659d6d30bf9b6338d9a481ca8fa4730c6b9
Asif <asifansari94@gmail.com>
2026-01-27 22:24:44 +0530

    Moved GameState class into model package under domain layer

--- app/src/main/java/com/example/tictactoeapp/presentation/viewmodel/GameBoardViewModel.kt
+++ app/src/main/java/com/example/tictactoeapp/presentation/viewmodel/GameBoardViewModel.kt
@@ -1,7 +1,7 @@
 package com.example.tictactoeapp.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
-import com.example.tictactoeapp.presentation.domain.GameState
+import com.example.tictactoeapp.presentation.domain.model.GameState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
