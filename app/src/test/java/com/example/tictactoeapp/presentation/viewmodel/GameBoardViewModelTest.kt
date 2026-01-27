5b81731b6280be894267ac349b0d0fc461293335
Asif <asifansari94@gmail.com>
2026-01-27 21:50:21 +0530

    Renamed GameBoardViewModelTest and modified testcase to test the board state flow define in GameBoardViewModel with turbine lib

---
+++ app/src/test/java/com/example/tictactoeapp/presentation/viewmodel/GameBoardViewModelTest.kt
@@ -0,0 +1,29 @@
+package com.example.tictactoeapp.presentation.viewmodel
+
+import app.cash.turbine.test
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+class GameBoardViewModelTest {
+
+    private lateinit var gameBoardViewModel: GameBoardViewModel
+
+    @Before
+    fun setup() {
+        gameBoardViewModel = GameBoardViewModel()
+    }
+
+    @Test
+    fun test_PlayerXClickOnCell_N() = runTest {
+        val tappedCellByPlayer = 2
+
+        gameBoardViewModel.board.test {
+            Assert.assertTrue(gameBoardViewModel.board.value[tappedCellByPlayer] == "")
+            gameBoardViewModel.play(tappedCellByPlayer)
+            Assert.assertTrue(awaitItem()[tappedCellByPlayer] == gameBoardViewModel.board.value[tappedCellByPlayer])
+            cancel()
+        }
+    }
+}
